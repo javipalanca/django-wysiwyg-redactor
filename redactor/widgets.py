9d2ff991b376d18dd611402837d4a571065ce41f
jpalanca <jpalanca@gmail.com>
2021-04-25 13:08:54 +0200

    reordered js imports

--- redactor/widgets.py
+++ redactor/widgets.py
@@ -42,9 +42,9 @@ class RedactorEditor(widgets.Textarea):
     def _media(self):
         _min = '' if settings.DEBUG else '.min'
         js = (
-            'redactor/jquery.redactor.init.js',
             'redactor/redactor{}.js'.format(_min),
             'redactor/langs/{}.js'.format(self.options.get('lang', 'en')),
+            'redactor/jquery.redactor.init.js',
         )
 
         if 'plugins' in self.options:
